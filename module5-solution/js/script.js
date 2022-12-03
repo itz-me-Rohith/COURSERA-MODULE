231155151345183eea9bcc36496ad5b9c5a12910
itz-me-Rohith <77039654+itz-me-Rohith@users.noreply.github.com>
2022-12-03 19:55:07 +0530

    Update script.js
    new

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -15,11 +15,11 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
 
     var homeHtmlUrl = "snippets/home-snippet.html";
     var allCategoriesUrl =
-        "https://davids-restaurant.herokuapp.com/categories.json";
+        "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
     var categoriesTitleHtml = "snippets/categories-title-snippet.html";
     var categoryHtml = "snippets/category-snippet.html";
     var menuItemsUrl =
-        "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+        "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
     var menuItemsTitleHtml = "snippets/menu-items-title.html";
     var menuItemHtml = "snippets/menu-item.html";
 
